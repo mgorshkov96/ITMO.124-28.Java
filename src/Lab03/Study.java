72b833a26b77231b61624578bae73d958bd21b36
Maksim Gorshkov <m.gorshkov96@gmail.com>
2023-03-05 22:35:01 +0300

    Completed lab03

---
+++ src/Lab03/Study.java
@@ -0,0 +1,15 @@
+package Lab03;
+
+public class Study {
+    private String course;
+
+    public Study(String course){
+        this.course = course;
+    }
+
+    public String printCourse() {
+        return this.course;
+    }
+}
+
+
